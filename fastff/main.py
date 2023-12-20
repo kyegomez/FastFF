431f45433deda17217da648302a3e794aa71b10b
Eternal Reclaimer <98760976+kyegomez@users.noreply.github.com>
2023-12-20 02:46:31 -0500

    Update main.py

--- fastff/main.py
+++ fastff/main.py
@@ -89,17 +89,17 @@ class FastFeedForward(nn.Module):
         return outputs
 
 
-# Example Usage
-input_dim = 768  # Example input dimension
-output_dim = 768  # Output dimension
-depth = 11  # Depth of the FFF tree
+# # Example Usage
+# input_dim = 768  # Example input dimension
+# output_dim = 768  # Output dimension
+# depth = 11  # Depth of the FFF tree
 
-# Create the Fast Feedforward module
-fast_ff = FastFeedForward(input_dim=input_dim, output_dim=output_dim, depth=depth)
+# # Create the Fast Feedforward module
+# fast_ff = FastFeedForward(input_dim=input_dim, output_dim=output_dim, depth=depth)
 
-# Example input tensor (batch_size, seq_len, input_dim)
-example_input = torch.randn(32, 128, input_dim)
+# # Example input tensor (batch_size, seq_len, input_dim)
+# example_input = torch.randn(32, 128, input_dim)
 
-# Forward pass
-output = fast_ff(example_input)
-print(output)
+# # Forward pass
+# output = fast_ff(example_input)
+# print(output)
